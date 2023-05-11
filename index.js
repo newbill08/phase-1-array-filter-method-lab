30b67ac3ebc884aecfda12a22998ecbbd1b110c4
joshua <josh.newbill08@hotmail.com>
2023-05-11 12:37:33 -0500

    completed findMatching, fuzzyMatch, matchName

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+
+function findMatching(driverArray,string){
+    return driverArray.filter((driver)=> driver.toLowerCase() === string.toLowerCase())
+};
+
+
+function fuzzyMatch(driverArray,string){
+    return driverArray.filter((driver) => driver.startsWith(string)
+    )
+};
+
+function matchName(driverArray,string){
+    return driverArray.filter((driver) => driver.name.toLowerCase() === string.toLowerCase())
+}
